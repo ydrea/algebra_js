6378de817ca5ade27da97eb6f7a0c85bb2c8351e
Igor Jevremović <ijevremovic@yahoo.com>
2025-01-03 13:00:10 +0100

    Add files via upload

--- vjezba62-react/src/components/Todo.jsx
+++ vjezba62-react/src/components/Todo.jsx
@@ -4,10 +4,24 @@ const Todo = ({ task, id, todo, todos, setTodos }) => {
   const deleteHandler = () => {
     setTodos(todos.filter((listElement) => listElement.id !== todo.id));
   };
+
+  const completedHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
   return (
     <div className="todo" key={id}>
-      <li className="todo-item">{task}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{task}</li>
+      <button className="complete-btn" onClick={completedHandler}>
         <FaCheck />
       </button>
       <button className="trash-btn" onClick={deleteHandler}>
